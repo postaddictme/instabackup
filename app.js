9bd5d1841000bf74007a0dc27eb94382a2febf25
r.kapishev <r.kapishev@samsung.com>
2016-02-15 18:45:28 +0300

    Set timeout for big accounts

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var logger = require('log4js').getLogger('app.js');
 var userController = require('./controllers/user');
 var app = express();
 var constants = require('./constants/constants');
-
+var timeout = require('connect-timeout');
 app.use(express.static('public'));
 app.use('/archives', express.static('account-medias/archive'));
-
+app.use(timeout('500s'));
 // create folder if does not exist
 fs.access(constants.FOLDER_ACCOUNT_MEDIAS, fs.R_OK | fs.W_OK, function (err) {
     if (err) {
@@ -63,6 +63,6 @@ function createFolders() {
 app.post('/api/checkUsername/:username', userController.checkUsername);
 app.post('/api/prepareMedias/:username', userController.prepareMedias);
 
-app.listen(process.env.PORT, function () {
+var server = app.listen(process.env.PORT, function () {
     console.log('Example app listening on port ' + process.env.PORT);
 });
