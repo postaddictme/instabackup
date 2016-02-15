aa65d6a0e305fe5df840f42b7e51082f33b05b66
r.kapishev <r.kapishev@samsung.com>
2016-02-15 10:13:41 +0300

    Create folders for media if does not exist

---
+++ constants/constants.js
@@ -0,0 +1,8 @@
+/**
+ * Created by raiym on 2/15/16.
+ */
+module.exports = Object.freeze({
+    FOLDER_ACCOUNT_MEDIAS: 'account-medias',
+    FOLDER_ACCOUNT_RAW_MEDIAS: 'account-medias/raw',
+    FOLDER_ACCOUNT_ARCHIVE_MEDIAS: 'account-medias/archive'
+});
