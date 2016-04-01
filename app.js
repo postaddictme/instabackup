5ad968bda5caa3d8307fe485606a27949a7b55b8
r.kapishev <r.kapishev@samsung.com>
2016-04-01 11:41:06 +0300

    Don't create folders on application start. Folders already exist

--- app.js
+++ app.js
@@ -8,58 +8,6 @@ var timeout = require('connect-timeout');
 app.use(express.static('public'));
 app.use('/archives', express.static('account-medias/archive'));
 app.use(timeout('600s'));
-// create folder if does not exist
-fs.access(constants.FOLDER_ACCOUNT_MEDIAS, fs.R_OK | fs.W_OK, function (err) {
-    if (err) {
-        logger.warn('Seems that folder does not exist.');
-        logger.debug('Creating folder', constants.FOLDER_ACCOUNT_MEDIAS);
-        fs.mkdir(constants.FOLDER_ACCOUNT_MEDIAS, function (err) {
-            if (err) {
-                logger.error('Failed to create folder.', constants.FOLDER_ACCOUNT_MEDIAS);
-                return;
-            }
-            logger.debug('Folder', constants.FOLDER_ACCOUNT_MEDIAS, 'created');
-            createFolders();
-        });
-        return;
-    }
-    createFolders();
-});
-
-function createFolders() {
-    fs.access(constants.FOLDER_ACCOUNT_RAW_MEDIAS, fs.R_OK | fs.W_OK, function (err) {
-        if (err) {
-            logger.warn(constants.FOLDER_ACCOUNT_RAW_MEDIAS, 'does not exist');
-            logger.debug('Creating', constants.FOLDER_ACCOUNT_RAW_MEDIAS);
-            fs.mkdir(constants.FOLDER_ACCOUNT_RAW_MEDIAS, function (err) {
-                if (err) {
-                    logger.error('An error occurred when trying to create ', constants.FOLDER_ACCOUNT_RAW_MEDIAS, err);
-                    return;
-                }
-                logger.debug(constants.FOLDER_ACCOUNT_RAW_MEDIAS, 'folder created.');
-            });
-            return;
-        }
-        logger.debug(constants.FOLDER_ACCOUNT_RAW_MEDIAS, 'folder exists.');
-    });
-
-    fs.access(constants.FOLDER_ACCOUNT_ARCHIVE_MEDIAS, fs.R_OK | fs.W_OK, function (err) {
-        if (err) {
-            logger.warn(constants.FOLDER_ACCOUNT_ARCHIVE_MEDIAS, 'does not exist');
-            logger.debug('Creating', constants.FOLDER_ACCOUNT_ARCHIVE_MEDIAS);
-            fs.mkdir(constants.FOLDER_ACCOUNT_ARCHIVE_MEDIAS, function (err) {
-                if (err) {
-                    logger.error('An error occurred when trying to create ', constants.FOLDER_ACCOUNT_ARCHIVE_MEDIAS, err);
-                    return;
-                }
-                logger.debug(constants.FOLDER_ACCOUNT_ARCHIVE_MEDIAS, 'folder created.');
-            });
-            return;
-        }
-        logger.debug(constants.FOLDER_ACCOUNT_ARCHIVE_MEDIAS, 'folder exists.');
-    });
-}
-
 app.post('/api/checkUsername/:username', userController.checkUsername);
 app.post('/api/prepareMedias/:username', userController.prepareMedias);
 
